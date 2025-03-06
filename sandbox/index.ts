b7922eea994c2ff5d8d6336c5cd85ae7d6d6370e
Sarthak <sarthakrawatbiz@gmail.com>
2025-03-06 17:01:35 +0530

    - Understanding hGetAll & hSet issues - Creating a user# hash in Redis

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,20 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950,
+		engine: { cylinders: 8 },
+		owner: null || '',
+		service: undefined || ''
+	});
+
+	const car = await client.hGetAll('car');
+
+	if (Object.keys(car).length === 0) {
+		console.log('No car found');
+	}
+	console.log(car);
+};
 run();
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,3 +1,5 @@
+import { usersKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
@@ -5,4 +7,13 @@ export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const id = genId();
+
+	await client.hSet(usersKey(id), {
+		username: attrs.username,
+		password: attrs.password
+	});
+
+	return id;
+};
